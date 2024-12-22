61efbcbe7a0beabd3f41952a41ac3443e277c9ea
Kevin Haninger <khaninger@gmail.com>
2024-12-22 23:57:29 +0100

    working, but total DOFs are 7 somehow

--- src/kinematics.rs
+++ src/kinematics.rs
@@ -1,23 +1,46 @@
 pub mod kinematics{
 
-  use rapier3d::prelude::*;
+    use rapier3d::prelude::*;
 
+    pub fn fwd_kin(bodies: &mut RigidBodySet,
+                   joints: &mut MultibodyJointSet,
+                   ee_link: MultibodyJointHandle,
+                   joint_angles: &[f32]
+    ) -> Isometry<f32> {
+        if let Some((multibody, link_id)) = joints.get_mut(ee_link) {
+            println!("multibody num links {:?} num dof {:?}",
+                     multibody.num_links(),
+                     multibody.ndofs()
+            );
+            multibody.apply_displacements(joint_angles);
+            multibody.forward_kinematics(&bodies,false);
+            multibody.update_rigid_bodies(bodies,false);
+        }
 
+        for (handle, body) in bodies.iter() {
+            println!("body {:?}", body.position());
+        }
+        
+        Isometry::identity()
+    }
 
-  pub struct Multibody {
-      bodies: RigidBodySet,
-      joints: MultibodyJointSet       
-  }
+    /*pub fn inverse_kinematics(joints: &MultibodyJointSet,
+                              bodies: &RigidBodySet,
+                              ee_link: &MultibodyLink,
+                              joint_angles: &[f32]
+    ) -> &[f32] {             
+        if let Some((multibody, link_id)) = joints.get_mut(ee_link) {
+            displacements = DVector::zeros(multibody.ndofs());
 
-  pub fn fwd_kin(bodies: &RigidBodySet,
-                 joints: &MultibodyJointSet,
-                 joint_angles: &[f32]
-  ) -> (Isometry<f32>) {     
-      for (joint_handle, angle) in joints.iter().zip(joint_angles.iter()) {
-          println!("anglearino {:?}", angle);
-      }
-
-      Isometry::identity()
-  }
-    
+            let options = InverseKinematicsOption::new();
+            multibody.inverse_kinematics(
+                &bodies,
+                ee_link,
+                &options,
+                &Isometry::identity(),
+                |_| true,
+                &mut displacements
+            );
+        }
+    }*/    
 }
--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,29 @@ use kinematics::kinematics::fwd_kin;
 
 fn main() {
     // The set that will contain our rigid-bodies.
-    let mut physics_pipeline = PhysicsPipeline::new();
     let mut bodies = RigidBodySet::new();
     let mut joints = MultibodyJointSet::new();
-    let mut collider_set = ColliderSet::new();
+    //let mut collider_set = ColliderSet::new();
+    //let mut physics_pipeline = PhysicsPipeline::new();
+    //let gravity = vector![0.0, 0.0, -9.81];
 
-    let rigid_body_1 = bodies.insert(RigidBodyBuilder::new(RigidBodyType::Dynamic)
-        .additional_mass_properties(MassProperties::new(
-            point![0.0, 1.0, 0.0],
-            0.5,
-            vector![0.3, 0.2, 0.1],
-        ))
-        .build());
+    let rigid_body_1 = bodies.insert(RigidBodyBuilder::fixed());
     
     let rigid_body_2 = bodies.insert(RigidBodyBuilder::new(RigidBodyType::Dynamic)
-        .additional_mass(0.2)
-        .build());
+                                     .additional_mass(0.2)
+                                     .can_sleep(false)
+                                     //.build()
+    );
 
     let ax_1 = Vector::x_axis();
     let jt_1 = RevoluteJointBuilder::new(ax_1)
-        .local_anchor1(point![0., 0., 0.])
-        .local_anchor2(point![0., 0., 0.]);
-    joints.insert(rigid_body_1, rigid_body_2, jt_1, true);
+        .local_anchor1(point![0., 0., 0.3])
+        .local_anchor2(point![0., 0., -0.3])
+        .build()
+        .data;
 
-    let gravity = vector![0.0, 0.0, -9.81];
-    fwd_kin(&bodies, &joints, &vec![0.1, 0.2]);
+    let ee_link = joints.insert(rigid_body_1, rigid_body_2, jt_1, true).unwrap();
+    
+    fwd_kin(&mut bodies, &mut joints, ee_link, &vec![0.2,0.3,0.,0.,0.,0., 0.1]);
     
 }
