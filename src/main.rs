2c10cf1a051577d199342d36d5f2ee75371f90c6
Kevin Haninger <khaninger@gmail.com>
2024-12-23 15:18:06 +0100

    added basic loading / fwd / inv.  Not sure that fwd kin is working yet

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,59 @@
+use std::path::Path;
+
 use rapier3d::prelude::*;
+use rapier3d_urdf::{UrdfRobot, UrdfLoaderOptions, UrdfMultibodyOptions};
 
 mod kinematics;
 use kinematics::kinematics::{fwd_kin, inv_kin};
 
-fn main() {
+fn main() {    
     let mut bodies = RigidBodySet::new();
     let mut multibody_joints = MultibodyJointSet::new();
+    let mut colliders = ColliderSet::new();
+
+    let urdf_path = Path::new("assets/fr3.urdf");
+    let urdf_options = UrdfLoaderOptions {
+        create_colliders_from_collision_shapes: false,
+        create_colliders_from_visual_shapes: false,
+        apply_imported_mass_props: true,
+        make_roots_fixed: true,
+        ..Default::default()
+    };
+    let (mut robot, _) = UrdfRobot::from_file(
+        urdf_path,
+        urdf_options,
+        None
+    ).unwrap();
+
+    println!("robot: {:#?}, njoints {:?}, nlinks {:?}", robot.links, robot.joints.len(), robot.links.len());
+
+    for jt in robot.joints.iter() {
+        println!("jt locked axes {:?}", jt.joint.locked_axes);
+    }
+
+    for lk in robot.links.iter() {
+        println!("link fixed {:?}", lk.body.is_fixed());
+    }
+
+    let handles = robot.insert_using_multibody_joints(&mut bodies, &mut colliders, &mut multibody_joints, UrdfMultibodyOptions::DISABLE_SELF_CONTACTS);
+
+    let multibody_handle = handles.joints.iter().last().unwrap().joint.unwrap();
     
-    let world = bodies.insert(RigidBodyBuilder::fixed().translation(vector![0.,0.,0.]));
-    let rigid_body_1 = bodies.insert(RigidBodyBuilder::dynamic().can_sleep(false));    
-    let rigid_body_2 = bodies.insert(RigidBodyBuilder::dynamic().can_sleep(false));
-
-    //let mut colliders = ColliderSet::new();
-    //let collider = ColliderBuilder::cuboid(0.2, 0.1, 0.2);
-    //colliders.insert_with_parent(collider, world, &mut bodies);
-    
-    let jt_world = RevoluteJointBuilder::new(Vector::x_axis())
-        .local_anchor1(point![0., 0., 0.1])
-        .local_anchor2(point![0., 0., 0.])
-        .build();
-    let jt_1 = RevoluteJointBuilder::new(Vector::x_axis())
-        .local_anchor1(point![0., 0., 0.1])
-        .local_anchor2(point![0., 0., 0.0])
-        .build();
-
-    multibody_joints.insert(world, rigid_body_1, jt_world, true);
-    let multibody_handle = multibody_joints.insert(rigid_body_1, rigid_body_2, jt_1, true).unwrap();
-
     println!("multibody_handle {:?}", multibody_handle);
     println!("multibody {:#?}", multibody_joints.get_mut(multibody_handle));
-    
+
     let jt_angles = inv_kin(&mut bodies,
                           &mut multibody_joints,
                           multibody_handle,
                           Isometry::identity()
     );
+    println!("jt angles: {:?}", jt_angles);
+
     let ee_pose = fwd_kin(&mut bodies,
                           &mut multibody_joints,
                           multibody_handle,
-                          rigid_body_2,
+                          handles.links.iter().last().unwrap().body,
                           jt_angles.as_slice());
-    println!("jt_angles: {:?}", jt_angles);
     println!("ee_pose:   {:?}", ee_pose);
+
 }
