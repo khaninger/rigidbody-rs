a68a618184bdb966c9c7b760bd434d407bd02f6b
Kevin Haninger <khaninger@gmail.com>
2024-12-23 14:02:38 +0100

    first draft, still have extra 6dof on first link but this might be from how body is constructed

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,45 @@
 use rapier3d::prelude::*;
 
 mod kinematics;
-use kinematics::kinematics::fwd_kin;
+use kinematics::kinematics::{fwd_kin, inv_kin};
 
 fn main() {
-    // The set that will contain our rigid-bodies.
     let mut bodies = RigidBodySet::new();
-    let mut joints = MultibodyJointSet::new();
-    //let mut collider_set = ColliderSet::new();
-    //let mut physics_pipeline = PhysicsPipeline::new();
-    //let gravity = vector![0.0, 0.0, -9.81];
-
-    let rigid_body_1 = bodies.insert(RigidBodyBuilder::fixed());
+    let mut multibody_joints = MultibodyJointSet::new();
     
-    let rigid_body_2 = bodies.insert(RigidBodyBuilder::new(RigidBodyType::Dynamic)
-                                     .additional_mass(0.2)
-                                     .can_sleep(false)
-                                     //.build()
+    let world = bodies.insert(RigidBodyBuilder::fixed().translation(vector![0.,0.,0.]));
+    let rigid_body_1 = bodies.insert(RigidBodyBuilder::dynamic().can_sleep(false));    
+    let rigid_body_2 = bodies.insert(RigidBodyBuilder::dynamic().can_sleep(false));
+
+    //let mut colliders = ColliderSet::new();
+    //let collider = ColliderBuilder::cuboid(0.2, 0.1, 0.2);
+    //colliders.insert_with_parent(collider, world, &mut bodies);
+    
+    let jt_world = RevoluteJointBuilder::new(Vector::x_axis())
+        .local_anchor1(point![0., 0., 0.1])
+        .local_anchor2(point![0., 0., 0.])
+        .build();
+    let jt_1 = RevoluteJointBuilder::new(Vector::x_axis())
+        .local_anchor1(point![0., 0., 0.1])
+        .local_anchor2(point![0., 0., 0.0])
+        .build();
+
+    multibody_joints.insert(world, rigid_body_1, jt_world, true);
+    let multibody_handle = multibody_joints.insert(rigid_body_1, rigid_body_2, jt_1, true).unwrap();
+
+    println!("multibody_handle {:?}", multibody_handle);
+    println!("multibody {:#?}", multibody_joints.get_mut(multibody_handle));
+    
+    let jt_angles = inv_kin(&mut bodies,
+                          &mut multibody_joints,
+                          multibody_handle,
+                          Isometry::identity()
     );
-
-    let ax_1 = Vector::x_axis();
-    let jt_1 = RevoluteJointBuilder::new(ax_1)
-        .local_anchor1(point![0., 0., 0.3])
-        .local_anchor2(point![0., 0., -0.3])
-        .build()
-        .data;
-
-    let ee_link = joints.insert(rigid_body_1, rigid_body_2, jt_1, true).unwrap();
-    
-    fwd_kin(&mut bodies, &mut joints, ee_link, &vec![0.2,0.3,0.,0.,0.,0., 0.1]);
-    
+    let ee_pose = fwd_kin(&mut bodies,
+                          &mut multibody_joints,
+                          multibody_handle,
+                          rigid_body_2,
+                          jt_angles.as_slice());
+    println!("jt_angles: {:?}", jt_angles);
+    println!("ee_pose:   {:?}", ee_pose);
 }
