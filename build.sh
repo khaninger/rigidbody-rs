8bf5a81a4a4b310b73387515fa93161bb0681d11
Kevin Haninger <khaninger@gmail.com>
2025-02-01 10:39:48 +0100

    hotfix: remove dead build script. Its nixified now

--- build.sh
+++
@@ -1,4 +0,0 @@
-# How to build and run the main.cpp linking against the rigidbody-rs lib
-cargo build --example rigidbody
-g++ -o main cpp/main.cpp -Ltarget/debug/examples -lrigidbody
-LD_LIBRARY_PATH=target/debug/examples ./main
