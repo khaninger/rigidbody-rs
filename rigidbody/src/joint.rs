70afb59a3d0b0a366430643fe6fceeb2aa4b9fec
Kevin Haninger <khaninger@gmail.com>
2025-02-18 00:29:55 +0100

    have refs working... tests are failing int he right way

--- rigidbody/src/joint.rs
+++ rigidbody/src/joint.rs
@@ -37,10 +37,6 @@ impl RevoluteJoint {
         self.parent*&self.joint_transform(q)
     }
 
-    pub fn parent_to_child_borrowed<'a> (&self, q: &'a Real) -> &'a Transform {
-        self.parent*&self.joint_transform(*q)
-    }
-    
     /// Transformation from parent coordinate system to the child
     #[inline(always)]
     pub fn child_to_parent(&self, q: Real) -> Transform {
@@ -52,6 +48,7 @@ impl RevoluteJoint {
     pub fn joint_transform(&self, q: Real) -> UnitQuaternion<Real> {
         UnitQuaternion::from_scaled_axis(self.axis.scale(q))
     }
+       
     
     pub fn from_xurdf_joint(
         joint: &Joint, link: &Link,
--- rigidbody/src/transforms.rs
+++ rigidbody/src/transforms.rs
@@ -23,12 +23,27 @@ use crate::inertia::Inertia;
 use crate::Multibody;
 
 #[derive(Debug)]
-pub struct Transforms<'a>([&'a Transform; 7]);
+pub struct Transfo<'a> {
+    mydata: &'a Real,
+}
 
-impl<'a> Transforms<'a> {
-    pub fn from_joints(mb: &Multibody, q: &'a[Real]) -> Self {
-        let arr: [Transform; 7] = zip(mb.iter(), q.iter())
-            .map(|(jt, &qi)| { jt.parent_to_child(qi)})
+impl <'a> Transfo<'a> {
+    pub fn from_angle(q: &'a Real) -> Transfo<'a> {
+        Transfo { mydata: q }
+    }
+
+    pub fn get_data(&self) -> &'a Real {
+        self.mydata
+    }
+}
+
+#[derive(Debug)]
+pub struct Transforms<'a>([Transfo<'a>; 7]);
+
+impl <'a> Transforms<'a> {
+    pub fn from_joints(mb: &Multibody, q: &'a [Real]) -> Self {
+        let arr: [Transfo; 7] = zip(mb.iter(), q.iter())
+            .map(|(jt, qi)| { Transfo::from_angle(qi) })//jt.parent_to_child(qi)})
             .collect::<Vec<_>>()
             .try_into()
             .expect("Build transforms");
@@ -43,15 +58,27 @@ mod test{
     use std::path::Path;
 
     #[test]
+    fn transfo () {
+        let mut t;
+        
+        {
+            let r = 0.1;
+            t = Transfo::from_angle(&r);
+            println!("{:?}", t.get_data());
+        }
+//        println!("{:?}", t.get_data());  // would cause error
+    }
+    
+    #[test]
     fn transforms () {
         let mb = Multibody::from_urdf(&Path::new("../assets/fr3.urdf"));
         let mut ts;
         {
-            let q = &[0.; 7];
-            ts = Transforms::from_joints(&mb, q);
+            let q = [0.1; 7];
+            ts = Transforms::from_joints(&mb, &q);
             println!("{:?}", ts);
         }
-        println!("{:?}", ts);
+  //      println!("{:?}", ts); // would cause error
         
     }
 }
